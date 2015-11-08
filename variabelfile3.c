53d4cef717fa87c569bf38473aba0da897f3ec3f
geraldzakwan <geraldi_dzakwan@yahoo.co.id>
2015-11-08 13:31:10 +0700

    Create variabelfile3.c
    File untuk menampung string tipe data (kota, pariwisata, start, dan lain-lain). Namun, belum digunakan karena pada implementasinya
    ditemukan error yang belum dapat diselesaikan.

---
+++ variabelfile3.c
@@ -0,0 +1,99 @@
+#include "mesinkata.h"
+
+void Daftar_Kata()
+{
+	Kata Kota;
+
+	Kota.Length = 4;
+	Kota.TabKata[0] = 'k';
+	Kota.TabKata[1] = 'o';
+	Kota.TabKata[2] = 't';
+	Kota.TabKata[3] = 'a';
+
+	Kata Pariwisata;
+
+	Pariwisata.Length = 10;
+	Pariwisata.TabKata[0] = 'p';
+	Pariwisata.TabKata[1] = 'a';
+	Pariwisata.TabKata[2] = 'r';
+	Pariwisata.TabKata[3] = 'i';
+	Pariwisata.TabKata[4] = 'w';
+	Pariwisata.TabKata[5] = 'i';
+	Pariwisata.TabKata[6] = 's';
+	Pariwisata.TabKata[7] = 'a';
+	Pariwisata.TabKata[8] = 't';
+	Pariwisata.TabKata[9] = 'a';
+
+	Kata Start;
+
+	Start.Length = 5;
+	Start.TabKata[0] = 's';
+	Start.TabKata[1] = 't';
+	Start.TabKata[2] = 'r';
+	Start.TabKata[3] = 'a';
+	Start.TabKata[4] = 't';
+
+	Kata Bonus;
+	
+	Bonus.Length = 5;
+	Bonus.TabKata[0] = 'b';
+	Bonus.TabKata[1] = 'o';
+	Bonus.TabKata[2] = 'n';
+	Bonus.TabKata[3] = 'u';
+	Bonus.TabKata[4] = 's';
+	
+	Kata Chance;
+	
+	Chance.Length = 6;
+	Chance.TabKata[0] = 'c';
+	Chance.TabKata[1] = 'h';
+	Chance.TabKata[2] = 'a';
+	Chance.TabKata[3] = 'n';
+	Chance.TabKata[4] = 'c';
+	Chance.TabKata[5] = 'e';
+	
+	Kata Penjara;
+	
+	Penjara.Length = 7;
+	Penjara.TabKata[0] = 'p';
+	Penjara.TabKata[1] = 'e';
+	Penjara.TabKata[2] = 'n';
+	Penjara.TabKata[3] = 'j';
+	Penjara.TabKata[4] = 'a';
+	Penjara.TabKata[5] = 'r';
+	Penjara.TabKata[6] = 'a';
+	
+	Kata Worldcup;
+	
+	Worldcup.Length = 8;
+	Worldcup.TabKata[0] = 'w';
+	Worldcup.TabKata[1] = 'o';
+	Worldcup.TabKata[2] = 'r';
+	Worldcup.TabKata[3] = 'l';
+	Worldcup.TabKata[4] = 'd';
+	Worldcup.TabKata[5] = 'c';
+	Worldcup.TabKata[6] = 'u';
+	Worldcup.TabKata[7] = 'p';
+	
+	Kata Worldtravel;
+	
+	Worldtravel.Length = 11;
+	Worldtravel.TabKata[0]='w';
+	Worldtravel.TabKata[1]='o';
+	Worldtravel.TabKata[2]='r';
+	Worldtravel.TabKata[3]='l';
+	Worldtravel.TabKata[4]='d';
+	Worldtravel.TabKata[5]='t';
+	Worldtravel.TabKata[6]='r';
+	Worldtravel.TabKata[7]='a';
+	Worldtravel.TabKata[8]='v';
+	Worldtravel.TabKata[9]='e';
+	Worldtravel.TabKata[10]='l';
+	
+	Kata Tax;
+	
+	Tax.Length = 3;
+	Tax.TabKata[0]='t';
+	Tax.TabKata[1]='a';
+	Tax.TabKata[2]='x';
+}
